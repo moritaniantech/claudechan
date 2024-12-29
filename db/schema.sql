56f5e5bc1401c96ef4bf881fef3d595a0fb4ba16
moritaniantech <mr.de.cube@gmail.com>
2024-12-29 22:30:58 +0900

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS chathistory (
   "channelId" TEXT NOT NULL,
   "timestamp" TEXT NOT NULL,
-  "threadTimestamp" TEXT NOT NULL,
+  "threadTimestamp" TEXT,
   "text" TEXT,
   "channelTimestamp" TEXT PRIMARY KEY NOT NULL
 );
